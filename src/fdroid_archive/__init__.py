f3f06922d22cb1e9c04a7361f7c33e766494fea2
yzqzss <yzqzss@yandex.com>
2024-09-15 17:12:52 +0800

    init

---
+++ src/fdroid_archive/__init__.py
@@ -0,0 +1,31 @@
+import argparse
+import subprocess
+import ruamel.yaml
+
+def get_pkgs():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('pkg', help='Package to archive', nargs='+')
+    args = arg_parser.parse_args()
+    return args.pkgs
+
+def main():
+    pkgs = get_pkgs()
+
+    for pkg in pkgs:
+        with open(f'metadata/{pkg}.yml') as f:
+            metadata = ruamel.yaml.YAML().load(f)
+
+        metadata['ArchivePolicy'] = 1
+        metadata['AutoUpdateMode'] = 'None'
+        metadata['UpdateCheckMode'] = 'None'
+
+        with open(f'metadata/{pkg}.yml', 'w') as f:
+            ruamel.yaml.YAML().dump(metadata, f)
+
+
+
+        subprocess.run(f'fdroid rewritemeta {pkg}', shell=True)
+        print(f'Archived metadata/{pkg}.yml')
+
+if __name__ == '__main__':
+    main()
